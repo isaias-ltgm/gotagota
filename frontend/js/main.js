80f4af3302c5e4b7cad5d6b42dbebde16bd2b111
isaias-ltgm <leo.diazmurrieta29@gmail.com>
2026-04-10 13:26:44 -0500

    jds

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -1,9 +1,11 @@
-// EVENTOS EN JAVASCRIPT
-document.addEventListener("DOMContentLoaded", () =>{
-    alert("jonas");
-    fetch('http://http://localhost:8080/api/clientes')
-  .then(response => response.json())
-  .then(data => 
-    console.log('Datos:' , data));
-    const elemento = document.getElementById
-})
+// EVENTOS EN JAVASCRIPT (CLICK, CARGAR, KEY,)
+document.addEventListener("DOMContentLoaded", () => {
+    fetch('http://localhost:8080/api/clientes')
+        .then(response => response.json())
+        .then(data => {
+            console.log('Datos:', data);
+            const elemento = document.getElementById("tabla-cliente")
+            elemento.innerHTML = JSON.stringify(data);
+            console.log(elemento)
+        })
+});
